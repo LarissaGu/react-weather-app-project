a03dccd16637d4aadc5187e6ba44f6b7beb81134
LarissaGu <68541012+LarissaGu@users.noreply.github.com>
2024-09-29 17:40:38 +0200

    Styling of Weather Info

--- src/Weather.js
+++ src/Weather.js
@@ -24,17 +24,19 @@ export default function Weather() {
           <div className="col-6">
             <h1>Berlin</h1>
             <ul>
-              <li>Saturday 28/09/2024 10:15</li>
+              <li>Saturday, 28/09/2024, 10:15</li>
               <li>Humidity: 56 %, Wind: 15 km/h</li>
             </ul>
           </div>
           <div className="col-6">
-            <img
-              src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
-              alt="Clear day"
-            ></img>
-            <span className="temperature">11 </span>
-            <span className="unit">°C</span>
+            <div className="temperature-container d-flex justify-content-end">
+              <img
+                src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
+                alt="Clear day"
+              ></img>
+              <span className="temperature">11 </span>
+              <span className="unit">°C</span>
+            </div>
           </div>
         </div>
       </div>
