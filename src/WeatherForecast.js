8a0bec88d6ca9f96ab8e49088724a1e03328d7f7
LarissaGu <68541012+LarissaGu@users.noreply.github.com>
2024-10-13 13:28:03 +0200

    First steps API integration

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,14 +1,26 @@
 import React from "react";
+import axios from "axios";
 import "./WeatherForecast.css";
 
 export default function WeatherForecast() {
+  function handleResponse(response) {
+    console.log(response);
+  }
+  let city = "Berlin";
+  let apiKey = "f1adaca67t6e03d488b4ca1d5710830o";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
         <div className="col">
           <div className="WeatherForecast-day">Mon</div>
           <div className="WeatherForecast-icon">
-            <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png" />
+            <img
+              src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
+              alt="icon"
+            />
           </div>
           <div className="WeatherForecast-temperature">
             <span className="WeatherForecast-temperature-max">18°</span>
