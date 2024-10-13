e0bd6834546f9ce8700f4efd602cf16f8d46af53
LarissaGu <68541012+LarissaGu@users.noreply.github.com>
2024-10-13 15:19:08 +0200

    Moved daily forecast in separate component

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
 
 export default function WeatherForecast(props) {
@@ -11,36 +12,12 @@ export default function WeatherForecast(props) {
     setLoaded(true);
   }
 
-  function day() {
-    let date = new Date(forecast[0].time * 1000);
-    let day = date.getDay();
-
-    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-
-    return days[day];
-  }
-
   if (loaded) {
     return (
       <div className="WeatherForecast">
         <div className="row">
           <div className="col">
-            <div className="WeatherForecast-day">{day()}</div>
-            <div className="WeatherForecast-icon">
-              <img
-                src={forecast[0].condition.icon_url}
-                alt={forecast[0].condition.icon}
-              />
-            </div>
-            <div className="WeatherForecast-temperature">
-              <span className="WeatherForecast-temperature-max">
-                {Math.round(forecast[0].temperature.maximum)}°
-              </span>
-              <span className="WeatherForecast-temperature-min">
-                {" "}
-                {Math.round(forecast[0].temperature.minimum)}°
-              </span>
-            </div>
+            <WeatherForecastDay data={forecast[0]} />
           </div>
         </div>
       </div>
---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function WeatherForecastDay(props) {
+  function day() {
+    let date = new Date(props.data.time * 1000);
+    let day = date.getDay();
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+  }
+  return (
+    <div className="WeatherForecastDay">
+      <div className="WeatherForecast-day">{day()}</div>
+      <div className="WeatherForecast-icon">
+        <img
+          src={props.data.condition.icon_url}
+          alt={props.data.condition.icon}
+        />
+      </div>
+
+      <div className="WeatherForecast-temperature">
+        <span className="WeatherForecast-temperature-max">
+          {Math.round(props.data.temperature.maximum)}°
+        </span>
+        <span className="WeatherForecast-temperature-min">
+          {" "}
+          {Math.round(props.data.temperature.minimum)}°
+        </span>
+      </div>
+    </div>
+  );
+}
